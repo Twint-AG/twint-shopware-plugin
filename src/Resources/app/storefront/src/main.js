9e2af1b07d6886b9487b9f6aa8c3a21aeaccecd9
Truong Vo <truong.vo@nfq.asia>
2024-05-15 09:33:47 +0700

    8-317 add app selector
    Fix
    
    Fix
    
    Fix

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,5 +1,5 @@
 // Import all necessary Storefront plugins
-
+import AppSelectorModalHandler from './app-selector-modal-handler/app-selector-modal-handler';
 // Register your plugin via the existing PluginManager
 const PluginManager = window.PluginManager;
-
+PluginManager.register('AppSelectorModalHandler', AppSelectorModalHandler, '[data-app-selector]');
