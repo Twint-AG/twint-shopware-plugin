09f5784a99bb65be5acf8838521df65f66de91d5
Tai Tran <tai@kosmonaut.io>
2024-06-03 22:51:56 +0700

    Make schedule classes compatible

--- src/ExpressCheckout/ScheduledTask/PairingMonitorTaskHandler.php
+++ src/ExpressCheckout/ScheduledTask/PairingMonitorTaskHandler.php
@@ -4,39 +4,54 @@ declare(strict_types=1);
 
 namespace Twint\ExpressCheckout\ScheduledTask;
 
-use Psr\Log\LoggerInterface;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTaskHandler;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Twint\ExpressCheckout\Service\Monitoring\MonitoringService;
 
-class PairingMonitorTaskHandler extends ScheduledTaskHandler
-{
-    private MonitoringService $service;
+$interface = 'Symfony\Component\Messenger\Handler\MessageSubscriberInterface';
+$interfaces = class_implements(ScheduledTaskHandler::class);
 
-    private LoggerInterface $logger;
-
-    public function __construct(
-        EntityRepository $scheduledTaskRepository,
-        LoggerInterface $logger
-    ) {
-        parent::__construct($scheduledTaskRepository);
-        $this->logger = $logger;
-    }
-
-    public static function getHandledMessages(): iterable
+if (interface_exists('Symfony\Component\Messenger\Handler\MessageSubscriberInterface') && in_array(
+    $interface,
+    $interfaces,
+    true
+)) {
+    class PairingMonitorTaskHandler extends ScheduledTaskHandler
     {
-        return [PairingMonitorTask::class];
+        private MonitoringService $service;
+
+        public static function getHandledMessages(): iterable
+        {
+            return [PairingMonitorTask::class];
+        }
+
+        public function setMonitoringService(MonitoringService $service): void
+        {
+            $this->service = $service;
+        }
+
+        public function run(): void
+        {
+            $this->service->monitor();
+        }
     }
-
-    public function setMonitoringService(MonitoringService $service): void
+} else {
+    #[AsMessageHandler(handles: PairingMonitorTask::class)]
+    class PairingMonitorTaskHandler extends ScheduledTaskHandler
     {
-        $this->service = $service;
-    }
+        private MonitoringService $service;
 
-    public function run(): void
-    {
-        $this->service->monitor();
+        public function setMonitoringService(MonitoringService $service): void
+        {
+            $this->service = $service;
+        }
 
-        $this->logger->info('Cron ran successfully');
+        public function run(): void
+        {
+            $this->service->monitor();
+
+            // @phpstan-ignore-next-line: compatible backwards compatibility
+            $this->exceptionLogger?->info('Cron ran successfully');
+        }
     }
 }
