21518c4965175b841c182bdbbc0a66d17512c2b0
Truong Vo <truong.vo@nfq.asia>
2024-06-18 11:34:56 +0700

    86 hide the custom field

---
+++ src/Resources/app/administration/src/module/sw-order/view/sw-order-detail-details/index.js
@@ -0,0 +1,40 @@
+import template from './sw-order-detail-details.html.twig';
+
+const { Criteria } = Shopware.Data;
+
+Shopware.Component.override('sw-order-detail-details', {
+    template,
+    inject: [
+        'repositoryFactory',
+        'acl',
+    ],
+    data() {
+        return {
+            customFieldSets: []
+        }
+    },
+    methods: {
+        createdComponent() {
+            this.$super('createdComponent');
+            this.customFieldSetRepository.search(this.customFieldSetCriteria).then((result) => {
+                this.customFieldSets = result;
+            });
+        }
+    },
+    computed: {
+        customFieldSetRepository() {
+            return this.repositoryFactory.create('custom_field_set');
+        },
+
+        customFieldSetCriteria() {
+            const criteria = new Criteria(1, null);
+            criteria.addFilter(Criteria.not(
+                'AND',
+                [Criteria.equals('name', 'twint_payment_custom_field_set')],
+            ));
+            criteria.addFilter(Criteria.equals('relations.entityName', 'order'));
+
+            return criteria;
+        }
+    }
+});
