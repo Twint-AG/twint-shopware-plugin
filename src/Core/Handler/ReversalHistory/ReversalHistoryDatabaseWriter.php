e1a6d8e0f91a7cd4c61832c212bf080f66736964
Truong Vo <truong.vo@nfq.asia>
2024-06-26 06:48:32 +0700

    86 add the api method & improve Admin UI

--- src/Core/Handler/ReversalHistory/ReversalHistoryDatabaseWriter.php
+++ src/Core/Handler/ReversalHistory/ReversalHistoryDatabaseWriter.php
@@ -4,26 +4,33 @@ declare(strict_types=1);
 
 namespace Twint\Core\Handler\ReversalHistory;
 
+use Exception;
+use Psr\Log\LoggerInterface;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 
 class ReversalHistoryDatabaseWriter implements ReversalHistoryWriterInterface
 {
     public function __construct(
-        private readonly EntityRepository $repository
+        private readonly EntityRepository $repository,
+        private readonly LoggerInterface $logger
     ) {
     }
 
     public function write(string $orderId, string $reversalId, float $amount, string $currency, string $reason): void
     {
-        $this->repository->create([
-            [
-                'orderId' => $orderId,
-                'reversalId' => $reversalId,
-                'amount' => $amount,
-                'currency' => $currency,
-                'reason' => $reason,
-            ],
-        ], Context::createDefaultContext());
+        try {
+            $this->repository->create([
+                [
+                    'orderId' => $orderId,
+                    'reversalId' => $reversalId,
+                    'amount' => $amount,
+                    'currency' => $currency,
+                    'reason' => $reason,
+                ],
+            ], Context::createDefaultContext());
+        } catch (Exception $e) {
+            $this->logger->error($e->getMessage());
+        }
     }
 }
