ff0e155ece0423122a34999fa64c0165c4c4a8e3
Khanh Pham <khanhpham@VNHCMLAP-0021.local>
2024-06-26 11:13:54 +0700

    init new branch

---
+++ cypress/e2e/pages/cartFlyout.ts
@@ -0,0 +1,8 @@
+const cartFlyout = {
+    goToCheckoutBtn: "a[href*=confirm].btn",
+    cartTwintExpressBtn: ".cart-offcanvas .twint-button",
+    goToCartPageLink: ".cart-offcanvas a[href*=cart].btn",
+    removeFromCartBtn: ".line-item-remove-button",
+    continueShoppingBtn: ".cart-offcanvas button.js-offcanvas-close"
+}
+export default cartFlyout
