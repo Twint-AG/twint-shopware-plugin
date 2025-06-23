bb241db674d1d21ad4c3e2facfc30eab64c73caa
Truong Vo <truong.vo@nfq.asia>
2025-06-24 02:47:28 +0700

    330 add the payment tracking
    Change

---
+++ src/Migration/Migration1750704424TransactionReport.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Twint\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+/**
+ * @internal
+ */
+#[Package('checkout')]
+class Migration1750704424TransactionReport extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1750704424;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $connection->executeStatement('
+            CREATE TABLE IF NOT EXISTS `twint_transaction_report` (
+                `order_transaction_id`         BINARY(16)     NOT NULL,
+                `order_transaction_version_id` BINARY(16)     NOT NULL,
+                `currency_iso`                 VARCHAR(3)     NOT NULL,
+                `total_price`                  DECIMAL(20, 2) NOT NULL,
+                `created_at`                   DATETIME(3)    NOT NULL,
+                `updated_at`                   DATETIME(3)    NULL,
+                PRIMARY KEY (`order_transaction_id`, `order_transaction_version_id`),
+                CONSTRAINT `fk.twint_transaction_report.order_transaction_id` FOREIGN KEY (`order_transaction_id`, `order_transaction_version_id`) REFERENCES `order_transaction` (`id`, `version_id`) ON DELETE CASCADE ON UPDATE CASCADE
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+        ');
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+    }
+}
---
+++ src/Reporting/DataAbstractionLayer/TransactionReport/TransactionReportCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Twint\Reporting\DataAbstractionLayer\TransactionReport;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
+use Shopware\Core\Framework\Log\Package;
+
+/**
+ * @internal
+ *
+ * @codeCoverageIgnore
+ *
+ * @extends EntityCollection<TransactionReportEntity>
+ */
+#[Package('checkout')]
+class TransactionReportCollection extends EntityCollection
+{
+    protected function getExpectedClass(): string
+    {
+        return TransactionReportEntity::class;
+    }
+}
