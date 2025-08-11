40f9a2da6e8880161d016b342780b81e622227f6
Tai Tran <ngoctai.tran@nfq.com>
2025-08-11 10:36:15 +0700

    Fix coding standard

--- src/Storefront/Controller/CheckoutController.php
+++ src/Storefront/Controller/CheckoutController.php
@@ -107,12 +107,12 @@ class CheckoutController extends StorefrontController
         } catch (Throwable $e) {
             $this->logger->error('TWINT start process error: ' . $e->getMessage());
             $this->addFlash(self::DANGER, $this->trans('twintPayment.error.pairingNotFound'));
-            
+
             return $this->json([
                 'completed' => true,
                 'orderId' => null,
                 'status' => PairingEntity::STATUS_FAILED,
-                'error-message' => $this->trans('twintPayment.error.paymentError')
+                'error-message' => $this->trans('twintPayment.error.paymentError'),
             ]);
         }
 
