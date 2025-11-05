dc914efce84416b04ee834e7fab618a16638642c
Tai Tran <ngoctai.tran@nfq.com>
2025-11-05 12:08:58 +0700

    Fix express checkout button issues on PLP pages

--- src/Extension/TwintExtension.php
+++ src/Extension/TwintExtension.php
@@ -10,11 +10,24 @@ use Twint\ExpressCheckout\Service\ExpressCheckoutButtonService;
 
 class TwintExtension extends AbstractExtension
 {
+    private bool $plpRendered = false;
+
     public function getFunctions(): array
     {
-        return [new TwigFunction('isTwintExpress', function (string $screen): bool {
-            return $this->button($screen);
-        })];
+        return [
+            new TwigFunction('isTwintExpress', function (string $screen): bool {
+                return $this->button($screen);
+            }),
+            new TwigFunction('isPlpRendered', function (): bool {
+                return $this->plpRendered;
+            }),
+            new TwigFunction('resetPlpRendered', function (): void {
+                $this->plpRendered = false;
+            }),
+            new TwigFunction('markPlpRendered', function (): void {
+                $this->plpRendered = true;
+            })
+        ];
     }
 
     public function button(string $screen): bool
