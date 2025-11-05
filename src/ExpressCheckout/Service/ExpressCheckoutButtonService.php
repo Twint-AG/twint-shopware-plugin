897cef2601e42211aa0ed975b91f4958c84fe419
Tai Tran <ngoctai.tran@nfq.com>
2025-11-05 12:08:58 +0700

    Fix express checkout button issues on PLP pages

--- src/ExpressCheckout/Service/ExpressCheckoutButtonService.php
+++ src/ExpressCheckout/Service/ExpressCheckoutButtonService.php
@@ -20,7 +20,6 @@ use Twint\ExpressCheckout\Util\PaymentMethodUtil;
 class ExpressCheckoutButtonService
 {
     private static ?array $buttons = null;
-
     public function __construct(
         private readonly PaymentMethodUtil $paymentMethodUtil,
         private readonly SettingServiceInterface $settingService,
